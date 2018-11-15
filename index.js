f1ddf9084af819595ee7ce3bb1ff532d4bf7de8c
Evyatar Alush <evyatar.a@fiverr.com>
2018-11-15 14:57:45 +0200

    A simpler way of getting the global object

--- index.js
+++ index.js
@@ -2,27 +2,10 @@ const WINDOW = 'window';
 const GLOBAL = 'global';
 
 /**
- * Fetches the global scope.
- * Checks for either "global" or "scope".
- * @return {Object}
- */
-const getGlobalScope = () => {
-    if (typeof global === 'object') {
-        return global;
-    }
-
-    if (typeof window === 'object') {
-        return window;
-    }
-
-    throw new Error('Couldn\'t fetch the global scope');
-};
-
-/**
  * The global scope, can be either "global" or "window".
  * @type {Object}
  */
-export const globalScope = getGlobalScope();
+export const globalScope = (new Function().constructor('return this'))();
 
 const checkGlobalObject = (globalObject) => new Function(`try {return this===${globalObject};}catch(e){ return false;}`)();
 
